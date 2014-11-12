b0e2928688e085bbdcb60bce248dccf280f36484
Namita Sabato <namitasabat@gmail.com>
2014-11-12 16:25:14 +0530

    next commit

--- TestGit/src/com/ts/testgit/MainActivity.java
+++ TestGit/src/com/ts/testgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.ts.testgit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("new user","");
 	}
 
 	@Override
